127339a59b8d2b94c4bd816514df646a656ebfec
Wizzard72 <paulslats@gmail.com>
2019-12-09 20:13:05 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -94,7 +94,7 @@ class BasePlugin:
             UpdateDevice(self.OUTSIDE_TEMP_UNIT, 0, "0.0")
             
         if (self.BURNING_HOURS_UNIT not in Devices):
-            Domoticz.Device(Name="Burning Hours", Unit=self.BURNING_HOURS_UNIT, TypeName='Custom').Create()
+            Domoticz.Device(Name="Burning Hours", Unit=self.BURNING_HOURS_UNIT, Type=243, Subtype=31).Create()
             UpdateDevice(self.BURNING_HOURS_UNIT, 0, "0.0")
             
         if (self.CH_SETPOINT_UNIT not in Devices):
