5253f17056834181b5bea3d45444331a35684a54
Wizzard72 <paulslats@gmail.com>
2019-12-13 09:19:21 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -247,20 +247,19 @@ class BasePlugin:
                 roomTemp = report['room_temp']
                 targetTemp = control['ch_mode_temp']
                 boilerStatus = int(report['boiler_status'])
-                Domoticz.Log('Atag One status retrieved: roomTemp='+str(roomTemp))
-                Domoticz.Log('Atag One status retrieved: ch_mode_temp='+str(targetTemp))
-                Domoticz.Log('Atag One status retrieved: boilerStatus='+str(boilerStatus))
+                Domoticz.Debug('Atag One status retrieved: roomTemp='+str(roomTemp))
+                Domoticz.Debug('Atag One status retrieved: ch_mode_temp='+str(targetTemp))
+                Domoticz.Debug('Atag One status retrieved: boilerStatus='+str(boilerStatus))
                 if ((boilerStatus & 8) == 8):
-                    Domoticz.Log('Updating with flame ON')
+                    Domoticz.Debug('Updating with flame ON')
                     UpdateDevice(self.TARGET_TEMP_UNIT, int(targetTemp), str(targetTemp), Images[self.FLAME_ON_IMG].ID)
                 else:
-                    Domoticz.Log('Updating with flame OFF')
+                    Domoticz.Debug('Updating with flame OFF')
                     UpdateDevice(self.TARGET_TEMP_UNIT, int(targetTemp), str(targetTemp), Images[self.FLAME_OFF_IMG].ID)
                 UpdateDevice(self.ROOM_TEMP_UNIT, int(roomTemp), str(roomTemp))
             else:
                 Domoticz.Log('Atag One invalid retrieve response (room_temp/ch_mode_temp/boiler_status)')
         else:
-            Domoticz.Log("Else")
             if (('acc_status' in response) and (int(response['acc_status']) == 3)):
                 self.hostAuth = False
                 newCountDown = 1
@@ -273,49 +272,49 @@ class BasePlugin:
             report = response['report']
             if ('room_temp' in report):
                 roomTemp = report['room_temp']
-                Domoticz.Log('Atag One status retrieved: roomTemp='+str(roomTemp))
+                Domoticz.Debug('Atag One status retrieved: roomTemp='+str(roomTemp))
                 UpdateDevice(self.ROOM_TEMP_UNIT, int(roomTemp), str(roomTemp))
             else:
                 Domoticz.Log('Atag One invalid retrieve response (room_temp)')
             if ('outside_temp' in report):
                 outsideTemp = report['outside_temp']
-                Domoticz.Log('Atag One status retrieved: outside_temp='+str(outsideTemp))
+                Domoticz.Debug('Atag One status retrieved: outside_temp='+str(outsideTemp))
                 UpdateDevice(self.OUTSIDE_TEMP_UNIT, int(outsideTemp), str(outsideTemp))
             else:
                 Domoticz.Log('Atag One invalid retrieve response (outside_temp)')
             if ('burning_hours' in report):
                 burningHours = report['burning_hours']
-                Domoticz.Log('Atag One status retrieved: burning_hours='+str(burningHours))
+                Domoticz.Debug('Atag One status retrieved: burning_hours='+str(burningHours))
                 UpdateDevice(self.BURNING_HOURS_UNIT, int(burningHours), str(burningHours))
             else:
                 Domoticz.Log('Atag One invalid retrieve response (burning_hours)')
             if ('ch_setpoint' in report):
                 chSetpoint = report['ch_setpoint']
-                Domoticz.Log('Atag One status retrieved: ch_setpoint='+str(chSetpoint))
+                Domoticz.Debug('Atag One status retrieved: ch_setpoint='+str(chSetpoint))
                 UpdateDevice(self.CH_SETPOINT_UNIT, int(chSetpoint), str(chSetpoint))
             else:
                 Domoticz.Log('Atag One invalid retrieve response (ch_setpoint)')
             if ('dhw_water_temp' in report):
                 dhwWaterTemp = report['dhw_water_temp']
-                Domoticz.Log('Atag One status retrieved: dhw_water_temp='+str(dhwWaterTemp))
+                Domoticz.Debug('Atag One status retrieved: dhw_water_temp='+str(dhwWaterTemp))
                 UpdateDevice(self.DHW_WATER_TEMP_UNIT, int(dhwWaterTemp), str(dhwWaterTemp))
             else:
                 Domoticz.Log('Atag One invalid retrieve response (dhw_water_temp)')
             if ('ch_water_temp' in report):
                 chWaterTemp = report['ch_water_temp']
-                Domoticz.Log('Atag One status retrieved: ch_water_temp='+str(chWaterTemp))
+                Domoticz.Debug('Atag One status retrieved: ch_water_temp='+str(chWaterTemp))
                 UpdateDevice(self.CH_WATER_TEMP_UNIT, int(chWaterTemp), str(chWaterTemp))
             else:
                 Domoticz.Log('Atag One invalid retrieve response (ch_water_temp)')
             if ('ch_water_pres' in report):
                 chWaterPres = report['ch_water_pres']
-                Domoticz.Log('Atag One status retrieved: dhw_water_pres='+str(chWaterPres))
+                Domoticz.Debug('Atag One status retrieved: dhw_water_pres='+str(chWaterPres))
                 UpdateDevice(self.CH_WATER_PRES_UNIT, int(chWaterPres), str(chWaterPres))
             else:
                 Domoticz.Log('Atag One invalid retrieve response (ch_water_pres)')
             if ('ch_return_temp' in report):
                 chReturnTemp = report['ch_return_temp']
-                Domoticz.Log('Atag One status retrieved: ch_return_temp='+str(chReturnTemp))
+                Domoticz.Debug('Atag One status retrieved: ch_return_temp='+str(chReturnTemp))
                 UpdateDevice(self.CH_RETURN_TEMP_UNIT, int(chReturnTemp), str(chReturnTemp))
             else:
                 Domoticz.Log('Atag One invalid retrieve response (ch_return_temp)')
@@ -372,7 +371,7 @@ class BasePlugin:
         return newCountDown
       
     def UpdateTargetTemp(self, target):
-        Domoticz.Debug("onConnect UpdateTargetTemp called")
+        Domoticz.Debug("UpdateTargetTemp called")
         Domoticz.Debug("target = "+str(target))
         self.setLevel = False        
         if (float(target) < self.TEMPERATURE_MIN) or (float(target) > self.TEMPERATURE_MAX):
